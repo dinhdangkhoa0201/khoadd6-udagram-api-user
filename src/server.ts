101af8f39e262ebfed139d0c2138736b0248df91
KhoaDD6 <dinhdangkhoa0201@gmail.com>
2022-08-19 22:26:31 +0700

    Change Access Control Port

--- src/server.ts
+++ src/server.ts
@@ -1,58 +1,64 @@
-import cors from 'cors';
-import express from 'express';
-import {sequelize} from './sequelize';
+import cors from "cors";
+import express from "express";
+import { sequelize } from "./sequelize";
 
-import {IndexRouter} from './controllers/v0/index.router';
+import { IndexRouter } from "./controllers/v0/index.router";
 
-import bodyParser from 'body-parser';
-import {config} from './config/config';
-import {V0_USER_MODELS} from './controllers/v0/model.index';
-
+import bodyParser from "body-parser";
+import { config } from "./config/config";
+import { V0_USER_MODELS } from "./controllers/v0/model.index";
 
 (async () => {
-  await sequelize.addModels(V0_USER_MODELS);
+    await sequelize.addModels(V0_USER_MODELS);
 
-  console.debug("Initialize database connection...");
-  await sequelize.sync();
+    console.debug("Initialize database connection...");
+    await sequelize.sync();
 
-  const app = express();
-  const port = process.env.PORT || 8080;
+    const app = express();
+    const port = process.env.PORT || 8080;
 
-  app.use(bodyParser.json());
+    app.use(bodyParser.json());
 
-  // We set the CORS origin to * so that we don't need to
-  // worry about the complexities of CORS this lesson. It's
-  // something that will be covered in the next course.
-  app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization',
-    ],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    origin: '*',
-  }));
+    // We set the CORS origin to * so that we don't need to
+    // worry about the complexities of CORS this lesson. It's
+    // something that will be covered in the next course.
+    app.use(
+        cors({
+            allowedHeaders: [
+                "Origin",
+                "X-Requested-With",
+                "Content-Type",
+                "Accept",
+                "X-Access-Token",
+                "Authorization",
+            ],
+            methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+            preflightContinue: true,
+            origin: "*",
+        })
+    );
 
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:58207");
-    res.header("Access-Control-Allow-Header", "Origin, X-Requested-Width, Content-Type, Accept, Authorization");
-    next();
-  })
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+        res.header("Access-Control-Allow-Origin", "http://127.0.0.1:52288");
+        res.header(
+            "Access-Control-Allow-Header",
+            "Origin, X-Requested-Width, Content-Type, Accept, Authorization"
+        );
+        next();
+    });
 
-  app.use('/api/v0/', IndexRouter);
+    app.use("/api/v0/", IndexRouter);
 
-  // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
-  } );
+    // Root URI call
+    app.get("/", async (req, res) => {
+        res.send("/api/v0/");
+    });
 
-
-  // Start the Server
-  app.listen( port, () => {
-    console.log( `server running ${config.url}` );
-    console.log( `server running on PORT: ${config.port}` );
-    console.log( `press CTRL+C to stop server` );
-  } );
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running ${config.url}`);
+        console.log(`server running on PORT: ${config.port}`);
+        console.log(`press CTRL+C to stop server`);
+    });
 })();
